b07e3fb41c8fb43294bee7f363467c5d308756d3
BapanBigData <bapanmldl7892@gmail.com>
2025-01-27 04:06:06 +0530

    changes pushed

--- app.py
+++ app.py
@@ -5,24 +5,32 @@ from src.components.predict import Prediction
 from fastapi import FastAPI, Request
 import uvicorn
 
-
+# Create a new instance of the FastAPI app
 app = FastAPI()
+
+# Mount the static directory for serving files
 app.mount("/static", StaticFiles(directory="static"))
+
+# Initialize Jinja2Templates for rendering HTML templates
 TEMPLATES = Jinja2Templates(directory='templates')
+
+# Initialize an empty list to store searched images
 searchedImages = []
 
+# Create a new instance of the Prediction class
 predict_pipe = Prediction()
 
-
+# Define the index route
 @app.get("/", status_code=200)
 @app.post("/")
 async def index(request: Request):
     """
     Description : This Route loads the index.html
     """
+    # Render the index.html template with the request object as context
     return TEMPLATES.TemplateResponse(name='index.html', context={"request": request})
 
-
+# Define the image upload route
 @app.post('/image')
 async def upload_file(file: UploadFile = File(...)):
     """
@@ -30,26 +38,38 @@ async def upload_file(file: UploadFile = File(...)):
     """
     global searchedImages, predict_pipe
     try:
+        # Check if the prediction pipeline is loaded
         if predict_pipe:
+            # Read the contents of the uploaded file
             contents = file.file.read()
+
+            # Run the predictions using the loaded pipeline
             searchedImages = predict_pipe.run_predictions(contents)
+
+            # Return a success message
             return {"message": "Prediction Completed"}
         else:
+            # If the pipeline is not loaded, return an error message
             return {"message": "First Load Model in Production using reload_prod_model route"}
     except Exception as e:
+        # Catch any exceptions and return an error message
         return {"message": f"There was an error uploading the file {e}"}
 
-
+# Define the reload route
 @app.post('/reload')
 def reload():
     """
     Description : This Route resets the predictions in a list for reload.
     """
     global searchedImages
+
+    # Reset the searched images list
     searchedImages = []
+
+    # Return a success message
     return
 
-
+# Define the reload prod model route
 @app.get('/reload_prod_model')
 def reload():
     """
@@ -58,22 +78,31 @@ def reload():
     """
     global predict_pipe
     try:
+        # Delete the existing prediction pipeline
         del predict_pipe
+
+        # Create a new instance of the Prediction class
         predict_pipe = Prediction()
+
+        # Return a success message
         return {"Response": "Successfully Reloaded"}
     except Exception as e:
+        # Catch any exceptions and return an error message
         return {"Response": e}
 
-
+# Define the gallery route
 @app.get('/gallery')
 async def gallery(request: Request):
     """
     Description : This Route lists all the predicted images on the gallery.html listing depends on prediction.
     """
     global searchedImages
+
+    # Render the gallery.html template with the request object and searched images list as context
     return TEMPLATES.TemplateResponse('gallery.html', context={"request": request, "length": len(searchedImages),
                                                             "searchedImages": searchedImages})
 
-
+# Run the app if this script is executed directly
 if __name__ == "__main__":
+    # Run the app on host 0.0.0.0 and port 8080
     uvicorn.run(app, host="0.0.0.0", port=8080)
